b147fd9f96e5a952354a809feff4ac43a1e2bf85
Ancient Record <general@tormechia.slmail.com>
2024-01-20 02:24:27 -0500

    first commit

---
+++ infiniteADWR.py
@@ -0,0 +1,66 @@
+import cv2
+from PIL import ImageGrab
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:\t %(message)s")
+
+
+def update_screenshot():
+    logging.info("Updating screenshot")
+    screenshot = ImageGrab.grab()
+    screenshot.save('images\\latest_screenshot.png','PNG')
+    logging.info("Screenshot updated")
+
+def is_image_present(image, template, threshold = 0.8):
+    """Check if smaller image is present within the larger image
+
+    Arguments:
+        smaller_image -- Image to be searched for. end_screen_template in this implementation
+        larger_image -- Image to be searched within. current_screenshot in this implementation
+        threshold -- Detection threshold for confidence of image being present
+    """    
+    logging.info("Checking for image match")
+    result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) #creates grayscale image showing pixel matches. probably
+    _, max_val, _, _ = cv2.minMaxLoc(result)
+    logging.info(f"Match value of {max_val}")
+    
+    if max_val > threshold:
+        logging.info("Image match success")
+        return True
+    else:
+        logging.info("Image match failed")
+        return False
+    
+#Setup
+seconds_interval = 5
+end_screen_template = cv2.imread("images\\end_indicator.png")
+current_screenshot = cv2.imread('images\\default_screenshot.png') #loads dummy screenshot until real one is taken
+
+while(True):
+    time.sleep(seconds_interval)
+    update_screenshot()
+    current_screenshot = cv2.imread("images\\latest_screenshot.png")
+    if is_image_present(current_screenshot, end_screen_template):
+        logging.info("Game has ended.")
+    else:
+        logging.info("Game is ongoing.")
+    
+
+
+def test():
+    end_screen_template = cv2.imread("images\\end_indicator.png")
+    assert end_screen_template is not None, "file could not be read, check with os.path.exists()"
+    current_screenshot = cv2.imread('images\\end_4player.png') 
+    #current_screenshot = cv2.imread('images\\end.png') 
+    #current_screenshot = cv2.imread('images\\test_nonend_screenshot.png') 
+    #current_screenshot = cv2.imread('images\\default_screenshot.png') 
+    assert current_screenshot is not None, "file could not be read, check with os.path.exists()"
+    
+    
+    if is_image_present(current_screenshot, end_screen_template):
+        print("End detected!")
+    else:
+        print("End NOT detected!")
+
+test()
