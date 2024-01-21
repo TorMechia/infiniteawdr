f46aa402ad520a6e97824cdc0dc6a996fc5c727b
Ancient Record <general@tormechia.slmail.com>
2024-01-21 12:56:00 -0500

    groundwork for random game params

--- main.py
+++ main.py
@@ -3,17 +3,49 @@ import time
 import logging
 import ahk
 import imageDetection
+import random
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:\t %(message)s")
 
 # todo: add params (random_cos = False, random_maps = False, random_weather = False, random_funds = False, random_income = False, ai_mode = normal)
 # Process for simple game restart
-def game_restart(special_type: str = None):
-    logging.info(f"Restarting game. Parameters: Special type: {special_type}")
-    if special_type is not None:
-        if special_type == "simple":
-            input_sequence = ["a", "a", "a", "a", "a", "a"]  # A x6
-            exec_inputs(input_sequence)
+def game_restart(restart_type: str = "simple"):
+    logging.info(f"Restarting game. Restart type: {restart_type}")
+    if restart_type == "simple": #restart with same game parameters
+        input_sequence = ["a", "a", "a", "a", "a", "a"]  # A x6
+        exec_inputs(input_sequence)
+    if restart_type == "random": #restart with randomized COs and map
+        random_game_restart()
+        
+        
+        
+def random_game_restart():
+    # start in map selection menu
+    new_map_number = random_map_number("fourP")
+    select_map(new_map_number)
+    # now in co menu
+    randomize_cos()
+    exec_inputs(['a','a'])
+    
+def random_map_number(map_type = "fourP"):
+    """Give random map number, 1 from 30"""
+    num_maps = None
+    if map_type == "fourP":
+        num_maps = 30
+    
+    return random.randint(1, 30)
+        
+def randomize_cos():
+    '''Picks a random CO for each cpu'''
+    #random number 1-12, then navigate using arrows
+    
+def select_map(target_map: int):
+    '''selects given map number in the menu, moving from current map'''
+    current_map = game_params["map_number"]
+
+    game_params["map_number"] = new_map_number
+    pass
+        
 
 
 def exec_inputs(input_sequence: list[str]):
@@ -75,5 +107,9 @@ def main(emulation_scale = "4x", seconds_between_checks = 5):
         else:
             logging.debug("Game is ongoing.")
 
+
+global game_params
+game_params = {"map_number":1, "cos":[0, 0, 0, 0]}
+#cos is the co number for each player 1-4, counting left to right in rows
 
 main()
